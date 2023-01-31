85aa94164b21372672c44b62258ff8080e3f2fc3
Gokul gopal <gokul1909@gmail.com>
2023-01-31 21:38:55 +0530

    Add project files.

---
+++ SASForBlobMVC/Controllers/BlobController.cs
@@ -0,0 +1,50 @@
+﻿using Azure.Storage.Blobs;
+using SASForBlobMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SASForBlobMVC.Controllers
+{
+    public class BlobController : Controller
+    {
+        // GET: Blob SAS url
+
+        private readonly BlobSAS _blobSAS;
+        public BlobController(BlobSAS blobSAS)
+        {
+            this._blobSAS = blobSAS;
+        }
+
+        BlobContainerClient blobContainerClient = CreateContainer("con1", false);
+
+        private static BlobServiceClient blobServiceClient;
+        private static BlobContainerClient CreateContainer(string containerName, bool isPublic)
+        {
+            //code for creating container
+            blobServiceClient = new BlobServiceClient(ConfigurationManager.AppSettings["blobconstr"]);
+            BlobContainerClient blobContainerClient = blobServiceClient.GetBlobContainerClient(containerName);
+
+            if (!blobContainerClient.Exists())
+            {
+                blobContainerClient.CreateIfNotExists();
+                Console.WriteLine("container created container_name");
+                if (isPublic)
+                {
+                    blobContainerClient.SetAccessPolicy(Azure.Storage.Blobs.Models.PublicAccessType.Blob);
+                }
+            }
+            return blobContainerClient;
+        }
+
+        public ActionResult Index()
+        {
+            CreateContainer("conatiner_name", true);
+            _blobSAS.CreateSASforBlob(blobContainerClient.GetBlobClient("blob_name"));
+            return View();
+        }
+    }
+}
---
+++ SASForBlobMVC/Models/BlobSAS.cs
@@ -0,0 +1,42 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Sas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SASForBlobMVC.Models
+{
+    public class BlobSAS
+    {
+        public string CreateSASforBlob(BlobClient blobClient, string storedPolicyName = null)
+        {
+            if (blobClient.CanGenerateSasUri)
+            {
+                BlobSasBuilder blobSasBuilder = new BlobSasBuilder()
+                {
+                    BlobContainerName = blobClient.BlobContainerName,
+                    BlobName = blobClient.Name,
+                    Resource = "b"
+                };
+                if (storedPolicyName == null)
+                {
+                    blobSasBuilder.ExpiresOn = DateTimeOffset.UtcNow.AddHours(1);
+                    blobSasBuilder.SetPermissions(BlobSasPermissions.Read | BlobSasPermissions.Read);
+                }
+                else
+                {
+                    blobSasBuilder.Identifier = storedPolicyName;
+                }
+                Uri sasUri = blobClient.GenerateSasUri(blobSasBuilder);
+                Console.WriteLine("Blob SAS uri" + sasUri);
+                Console.ReadKey();
+                return sasUri.ToString();
+               
+            }
+
+            return "SAS url not generated";
+            
+        }
+    }
+}
